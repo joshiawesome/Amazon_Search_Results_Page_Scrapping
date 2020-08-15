2427a8db0155aa6964d31c08655e1f623a7ee809
joshiawesome <30716040+joshiawesome@users.noreply.github.com>
2020-08-15 12:56:41 +0530

    Add files via upload

---
+++ SearchResults.py
@@ -0,0 +1,55 @@
+from selectorlib import Extractor
+import requests
+import json
+from time import sleep
+
+#to read the YAML file
+e=Extractor.from_yaml_file('SearchResults.yml')
+
+def scrape(url):
+    
+#metadata headers to request webpage/data from the Amazon website
+    headers = {
+        'dnt': '1',
+        'upgrade-insecure-requests':'1',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-user': '?1',
+        'sec-fetch-dest': 'document',
+        'referer': 'https://www.amazon.com/',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    }
+
+#sending a HTTP request to the server and dowloading the webpage
+    print("Creating Json output file containing scrapped data %s"%url)
+    r=requests.get(url, headers=headers)
+    
+#using status code to check whether the page was blocked by Amazon
+    if r.status_code>500:
+        if "For access to Amazon data:" in r.text:
+            print("Page %s was blocked by Amazon. Use better proxies\n"%url)
+        else:
+            print("Page %s must have been blocked by Amazon as the status code was %d"%(url,r.status_code))
+        return None
+    return e.extract(r.text)
+
+#opening the urllist in "read mode" and a new json output fie to write the scrapped data into in "write mode"
+with open("search_urls.txt",'r') as urllist, open('Output.jsonl','w') as outfile:
+#reading in urls from the urllist and passing to the "scrap function" for retrieval of data from the urls.
+    for url in urllist.read().splitlines():
+        data = scrape(url)
+#storing the data
+        if data:
+            for product in data['products']:
+                product['search_url'] = url
+                print("Saving Product: %s"%product['title'])
+#writing scrapped data into the json file and displaying data on seperate lines
+                json.dump(product,outfile)
+                outfile.write("\n")
+
+    
+    
+        
+        
